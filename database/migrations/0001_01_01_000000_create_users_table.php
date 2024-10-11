47f55ca923518a584cab8587ee2f0f8f970e45bf
BiLLY <pberzosa83@gmail.com>
2024-10-11 12:21:24 +0200

    Migrations, Models y Controllers OK

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('dni')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone');
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
 
---
+++ database/migrations/2024_10_11_094146_create_analyses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('analyses', function (Blueprint $table) {
+            $table->id();
+            $table->date('analysis_date');
+            $table->decimal('acidity', 5, 2);
+            $table->decimal('humidity', 5, 2);
+            $table->decimal('yield', 5, 2);
+            $table->unsignedBigInteger('entry_id');
+            $table->unsignedBigInteger('worker_id');
+            $table->unsignedBigInteger('oil_id');
+            $table->decimal('oil_quantity', 10, 2);
+            $table->foreign('entry_id')->references('id')->on('entries')->onDelete('cascade');
+            $table->foreign('worker_id')->references('worker_id')->on('workers');
+            $table->foreign('oil_id')->references('id')->on('oils');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('analyses');
+    }
+};
