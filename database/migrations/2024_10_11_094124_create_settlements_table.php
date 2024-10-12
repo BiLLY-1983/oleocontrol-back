2159bdc2368bb22006395a90f3c3a880bcecf20f
BiLLY <pberzosa83@gmail.com>
2024-10-12 19:30:15 +0200

    PRECIO nullable en LIQUIDACIONES

--- database/migrations/2024_10_11_094124_create_settlements_table.php
+++ database/migrations/2024_10_11_094124_create_settlements_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('settlements', function (Blueprint $table) {
             $table->id();
             $table->decimal('amount', 10, 2);
-            $table->decimal('price', 10, 2);
+            $table->decimal('price', 10, 2)->nullable();
             $table->enum('settlement_status', ['Pendiente', 'Aceptada', 'Cancelada'])->default('Pendiente');
             $table->unsignedBigInteger('member_id');
             $table->unsignedBigInteger('worker_id')->nullable();
