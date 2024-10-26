c72951cd868cb91d32dace9608090ecce61a3fbe
BiLLY <pberzosa83@gmail.com>
2024-10-26 21:26:59 +0200

    Edicion de control de entradas

--- routes/api/entries.php
+++ routes/api/entries.php
@@ -9,13 +9,13 @@ use Illuminate\Support\Facades\Route;
 
 // Rutas para administradores
 Route::middleware('admin')->group(function () {
-    Route::get('/entries', [EntryController::class, 'index']);
     Route::get('/entries/{entryId}', [EntryController::class, 'show']);
     Route::delete('/entries/{entryId}', [EntryController::class, 'destroy']);
 });
 
 // Rutas para empleados (y administradores)
 Route::middleware('department:Control de entradas')->group(function () {
+    Route::get('/entries', [EntryController::class, 'index']);
     Route::post('/entries', [EntryController::class, 'store']);
     Route::put('/entries/{entryId}', [EntryController::class, 'update']);
 });
